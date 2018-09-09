51855f5d621018c75c08cd20cc25a94f82e2028c
Craig McIntyre <craig@thebirdsbeak.com>
2018-09-09 23:38:04 +0100

    Added fading hour bars

--- binary_clock/binary_clock.ino
+++ binary_clock/binary_clock.ino
@@ -6,6 +6,8 @@ Beerware by Craig McIntyre (thebirdsbeak.com)
 With grateful thanks to those responsible for the DS3231.h library.
 */
 
+unsigned long previousMillis = 0;
+const long interval = 50;
 
 #include <Wire.h>
 #include "DS3231.h"
@@ -15,17 +17,17 @@ DS3231 Clock;
 byte hr;
 byte mn;
 
-// byte set_hour = 4; // Uncomment to set time
-// byte set_min = 43; // Uncomment to set time
+// byte set_hour = 05; // Uncomment to set time
+// byte set_min = 34; // Uncomment to set time
 
 const byte numMinPins = 6;
 const byte numHrPins = 4;
-byte minPins[] = {4, 5, 6, 7, 8, 9};
-byte hrPins[] = {10, 11, 12, 13};
+byte minPins[] = {4, 5, 7, 8, 12, 13};
+byte hrPins[] = {6, 9, 10, 11};
 
 void setup() {
 
-//    Serial.begin(57600); // Uncomment for debugging
+    Serial.begin(57600); // Uncomment for debugging
     Wire.begin();
 
   for (int thisPin = 0; thisPin < numMinPins; thisPin++) {
@@ -36,11 +38,14 @@ void setup() {
     pinMode(hrPins[thisPin], OUTPUT);
   }
 
-    Clock.setClockMode(true);  // set to 12h time (not 24h)
+//    Clock.setClockMode(true);  // set to 12h time (not 24h)
 //    Clock.setHour(set_hour); // Uncomment to set hour
 //    Clock.setMinute(set_min); //Uncomment to set minute
   
 }
+
+
+
 
 void loop() {
 
@@ -48,30 +53,34 @@ void loop() {
     hr = now.hour(), DEC;
     mn = now.minute(), DEC;
 
-//    Serial.print(hr); // Uncomment for debugging
-//    Serial.print(":"); // Uncomment for debugging
-//    Serial.println(mn); // Uncomment for debugging
+    Serial.print(hr); // Uncomment for debugging
+    Serial.print(":"); // Uncomment for debugging
+    Serial.println(mn); // Uncomment for debugging
+
  
   for (byte i=0; i<numMinPins; i++) {
     byte state = bitRead(mn, i);
     digitalWrite(minPins[i], state);
   }
-  
+
+delay(1000);
   for (byte i=0; i<numHrPins; i++) {
-    byte state = bitRead(hr, i);
-    digitalWrite(hrPins[i], state);
+        byte state = bitRead(hr, i);
+        if (state == 1) {
+  for (int fadeValue = 0 ; fadeValue <= 250; fadeValue += 5) {
+    analogWrite(hrPins[i], fadeValue);
+    delay(20);
+  }
+  }
   }
 
-    delay(500);
-
   for (byte i=0; i<numHrPins; i++) {
-    digitalWrite(hrPins[i], LOW);
+            byte state = bitRead(hr, i);
+        if (state == 1) {
+  for (int fadeValue = 250 ; fadeValue >= 0; fadeValue -= 5) {
+    analogWrite(hrPins[i], fadeValue);
+    delay(20);
   }
-
-    delay(500);
-
-  for (byte i=0; i<numHrPins; i++) {
-    byte state = bitRead(hr, i);
-    digitalWrite(hrPins[i], state);
+  }
   }
 }
