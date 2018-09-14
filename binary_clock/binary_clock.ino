e623299f041c0b5ca1d09db11fe144f118329da7
Craig McIntyre <craig@thebirdsbeak.com>
2018-09-14 18:55:41 +0100

    Finalised code

--- binary_clock/binary_clock.ino
+++ binary_clock/binary_clock.ino
@@ -6,9 +6,6 @@ Beerware by Craig McIntyre (thebirdsbeak.com)
 With grateful thanks to those responsible for the DS3231.h library.
 */
 
-unsigned long previousMillis = 0;
-const long interval = 50;
-
 #include <Wire.h>
 #include "DS3231.h"
 RTClib RTC;
@@ -17,12 +14,12 @@ DS3231 Clock;
 byte hr;
 byte mn;
 
-// byte set_hour = 05; // Uncomment to set time
-// byte set_min = 34; // Uncomment to set time
+ byte set_hour = 06; // Uncomment to set time
+ byte set_min = 52; // Uncomment to set time
 
 const byte numMinPins = 6;
 const byte numHrPins = 4;
-byte minPins[] = {4, 5, 7, 8, 12, 13};
+byte minPins[] = {3, 4, 5, 7, 8, 12};
 byte hrPins[] = {6, 9, 10, 11};
 
 void setup() {
@@ -30,21 +27,19 @@ void setup() {
     Serial.begin(57600); // Uncomment for debugging
     Wire.begin();
 
-  for (int thisPin = 0; thisPin < numMinPins; thisPin++) {
+    for (int thisPin = 0; thisPin < numMinPins; thisPin++) {
     pinMode(minPins[thisPin], OUTPUT);
-  }
+    }
 
-  for (int thisPin = 0; thisPin < numHrPins; thisPin++) {
+    for (int thisPin = 0; thisPin < numHrPins; thisPin++) {
     pinMode(hrPins[thisPin], OUTPUT);
-  }
+    }
 
-//    Clock.setClockMode(true);  // set to 12h time (not 24h)
-//    Clock.setHour(set_hour); // Uncomment to set hour
-//    Clock.setMinute(set_min); //Uncomment to set minute
-  
+    Clock.setClockMode(true);  // set to 12h time (not 24h)
+    Clock.setHour(set_hour); // Uncomment to set hour
+    Clock.setMinute(set_min); //Uncomment to set minute 
+
 }
-
-
 
 
 void loop() {
@@ -58,29 +53,30 @@ void loop() {
     Serial.println(mn); // Uncomment for debugging
 
  
-  for (byte i=0; i<numMinPins; i++) {
-    byte state = bitRead(mn, i);
-    digitalWrite(minPins[i], state);
-  }
+    for (byte i=0; i<numMinPins; i++) {
+      byte state = bitRead(mn, i);
+      digitalWrite(minPins[i], state);
+    }
 
-delay(1000);
-  for (byte i=0; i<numHrPins; i++) {
-        byte state = bitRead(hr, i);
-        if (state == 1) {
-  for (int fadeValue = 0 ; fadeValue <= 250; fadeValue += 5) {
-    analogWrite(hrPins[i], fadeValue);
-    delay(20);
-  }
-  }
-  }
+    delay(1000);
+    for (byte i=0; i<numHrPins; i++) {
+      byte state = bitRead(hr, i);
+      if (state == 1) {
+        for (int fadeValue = 0 ; fadeValue <= 250; fadeValue += 5) {
+          analogWrite(hrPins[i], fadeValue);
+          delay(20);
+        }
+      }
+    }
 
-  for (byte i=0; i<numHrPins; i++) {
-            byte state = bitRead(hr, i);
-        if (state == 1) {
-  for (int fadeValue = 250 ; fadeValue >= 0; fadeValue -= 5) {
-    analogWrite(hrPins[i], fadeValue);
-    delay(20);
-  }
-  }
-  }
+    for (byte i=0; i<numHrPins; i++) {
+      byte state = bitRead(hr, i);
+      if (state == 1) {
+        for (int fadeValue = 250 ; fadeValue >= 0; fadeValue -= 5) {
+          analogWrite(hrPins[i], fadeValue);
+          delay(20);
+        }
+      }
+    }
+
 }
